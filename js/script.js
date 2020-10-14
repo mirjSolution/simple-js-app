21f5706cd7e019bf7313a9bcf9c646f68f6f7732
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-14 09:33:55 -0400

    Add a new array of objects on scripts.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-alert('Hello World');
 
-let favoriteFood = 'Fried Chicken';
+// array of objects
+let pokemonList = [
+    { name: 'Bulbasur', height: '7', types: ['grass', 'poison']}, 
+    { name: 'Charmeleon', height: '1', types: ['fire', 'water']}, 
+    { name: 'Blastoise', height: '8', types: ['water', 'air']}, 
+];
 
-document.write(favoriteFood)
