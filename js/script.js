bab855057ceee5c2ca933c4ca57bf05d3d08424e
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-15 18:24:34 -0400

    Add show details function and refactor other function to more readable

--- js/script.js
+++ js/script.js
@@ -6,32 +6,49 @@ let pokemonList = [
     { name: 'Blastoise', height: 8, types: ['water', 'air']}, 
 ];
 
-// IIFE pokemonRepository
+// Function to add pokemon
+function add(pokemon) {
+    pokemonList.push(pokemon)
+}
+
+// Function to view all pokemon on the array
+function getAll() {
+    return pokemonList;
+}
+
+// Function to log the name of pokemon to console
+function showDetails(pokemon) {
+    console.log(pokemon.target.outerText);
+}
+
+// Function to create button and display it on the screen
+function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    button.addEventListener('click', showDetails)
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+}
+
+// IIFE
 let pokemonRepository = (function () {
    return {
-      add: function(pokemon) {
-        pokemonList.push(pokemon);
-      },
-      getAll: function() {
-        return pokemonList;
-      }
+      add: add,
+      getAll: getAll,
+      addListItem: addListItem,
     };
   })();
 
+  // Add pokemon on array
   pokemonRepository.add({ name: 'Pikachu', height: 5, types:['electric', 'galvanic'] });
-  console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
 
-  // IIFE function with loop to pokemonList array and display it on the browser
-(function () {
-    pokemonList.forEach(function(pokemon) {
-        let pokemonName = pokemon.name
-        let pokemonHeight = pokemon.height
-        // condition to add wow that's big if height is greater than 7
-        if(pokemonHeight > 5) {
-            document.write(pokemonName + ' (height : ' + pokemonHeight + ')' + ' - Wow, that\'s big!'); 
-        } else {
-            document.write(pokemonName + ' (height : ' + pokemonHeight + ')'); 
-        }
-            document.write('<br>', '<br>');
-        });
-})();
+  // Loop to the array and display
+  pokemonRepository.getAll().forEach(function (pokemon) {
+     pokemonRepository.addListItem(pokemon);
+  })
+
+
+
