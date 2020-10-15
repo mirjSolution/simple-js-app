832567fcb7d2740eef9e9c58bac0ba2b7b32f9ea
mirjSolution <ricojohndatoon23@gmail.com>
2020-10-15 13:10:39 -0400

    write a for loop in pokemonList array and a conditon that if height is greater that 5 concatenate a message Wow thats big

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 
 // array of objects
 let pokemonList = [
-    { name: 'Bulbasur', height: '7', types: ['grass', 'poison']}, 
-    { name: 'Charmeleon', height: '1', types: ['fire', 'water']}, 
-    { name: 'Blastoise', height: '8', types: ['water', 'air']}, 
+    { name: 'Bulbasur', height: 9, types: ['grass', 'poison']}, 
+    { name: 'Charmeleon', height: 4, types: ['fire', 'water']}, 
+    { name: 'Blastoise', height: 8, types: ['water', 'air']}, 
 ];
 
+// loop to pokemonList array and display it on the browser
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemonListName = pokemonList[i].name
+    let pokemonListHeight = pokemonList[i].height
+    // condition to add wow that's big if height is greater than 7
+    if(pokemonListHeight > 5) {
+        document.write(pokemonListName + ' (height : ' + pokemonListHeight + ')' + ' - Wow, that\'s big!'); 
+    } else {
+        document.write(pokemonListName + ' (height : ' + pokemonListHeight + ')'); 
+    }
+    document.write('<br>', '<br>');
+  }
